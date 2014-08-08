ca30b0e8d35a7d8e353657276792de086ac2ce2b
mobileappscloud <doc92606@gmail.com>
2014-08-08 05:08:58 -0700

    Initial Commit

---
+++ ICOI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ICOI
+//
+//  Created by iMac on 8/8/14.
+//  Copyright (c) 2014 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ICOI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ICOI
+//
+//  Created by iMac on 8/8/14.
+//  Copyright (c) 2014 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
